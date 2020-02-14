18cbb931cb409ac04f6f6859d2c117a28958c3ec
deganoth <oliver.deegan@gmail.com>
2020-02-14 12:00:22 +0000

    checkout added with dymanic cart options for quantity updates

--- cart/contexts.py
+++ cart/contexts.py
@@ -8,11 +8,13 @@ def cart_contents(request):
     cart =  request.session.get('cart', {})
     
     cart_items = []
+    sub_total = 0
     total = 0
     product_count = 0
     
     for id, quantity in cart.items():
         product = get_object_or_404(Product, pk=id)
+        sub_total = quantity * product.price
         total += quantity * product.price
         product_count += quantity
         cart_items.append({'id': id, 'quantity': quantity, 'product': product})
