0d084466ade42cc1a2d9628761c61224d947795e
deganoth <oliver.deegan@gmail.com>
2020-02-29 16:13:37 +0000

    initial commit

--- mushop_main/settings.py
+++ mushop_main/settings.py
@@ -92,14 +92,14 @@ WSGI_APPLICATION = 'mushop_main.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 
-# DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         }
+# }
 
 
 
