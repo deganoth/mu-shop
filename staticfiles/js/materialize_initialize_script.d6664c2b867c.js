71bdc22d0f25d71b3a0914091a964d6d4d07d8c8
deganoth <oliver.deegan@gmail.com>
2020-03-08 18:20:33 +0000

    file cleanup

--- staticfiles/js/materialize_initialize_script.d6664c2b867c.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	$('.sidenav').sidenav();
-	$('.collapsible').collapsible();
-	$(".dropdown-trigger").dropdown();
-	$('.carousel.carousel-slider').carousel({
-		fullWidth: true,
-		indicators: true
-	});
-});
